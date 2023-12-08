088260f4220b344abdee34bbc273348fe7e4ce17
Bredariolcaroline <bredariolcaroline@gmail.com>
2023-12-08 12:48:32 -0300

    Create auxiliary_functions.py

---
+++ auxiliary_functions.py
@@ -0,0 +1,27 @@
+def inspect_outliers(dataframe, column, whisker_width=1.5):
+    """Função para inspecionar outliers.
+
+    Parameters
+    ----------
+    dataframe : pandas.DataFrame
+        Dataframe com os dados.
+    column : List[str]
+        Lista com o nome das colunas (strings) a serem utilizadas.
+    whisker_width : float, opcional
+        Valor considerado para detecção de outliers, por padrão 1.5
+
+    Returns
+    -------
+    pd.DataFrame
+        Dataframe com os outliers.
+    """
+
+    q1 = dataframe[column].quantile(0.25)
+    q3 = dataframe[column].quantile(0.75)
+    iqr = q3 - q1
+    lower_bound = q1 - whisker_width * iqr
+    upper_bound = q3 + whisker_width * iqr
+
+    return dataframe[
+        (dataframe[column] < lower_bound) | (dataframe[column] > upper_bound)
+    ]
